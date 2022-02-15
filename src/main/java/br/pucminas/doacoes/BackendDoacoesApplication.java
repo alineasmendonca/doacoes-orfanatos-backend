56aec7c46f1cafe15994942276cd5de22c56471b
alineasmendonca <aline.alves.silva.mendonca@gmail.com>
2022-02-15 00:46:51 -0300

    Primeira versao do projeto Doacoes para orfanatos

---
+++ src/main/java/br/pucminas/doacoes/BackendDoacoesApplication.java
@@ -0,0 +1,13 @@
+package br.pucminas.doacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDoacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDoacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/pucminas/doacoes/BackendDoacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucminas.doacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDoacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
