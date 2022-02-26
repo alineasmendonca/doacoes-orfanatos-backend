0e9d66fec6b6d8bccbdf27651969ed2c96d2fba8
alineasmendonca <aline.alves.silva.mendonca@gmail.com>
2022-02-26 13:52:39 -0300

    Update categoria

--- src/main/java/br/pucminas/doacoes/services/CategoriaService.java
+++ src/main/java/br/pucminas/doacoes/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.pucminas.doacoes.domain.Categoria;
+import br.pucminas.doacoes.dtos.CategoriaDTO;
 import br.pucminas.doacoes.repositories.CategoriaRepository;
 import br.pucminas.doacoes.services.exceptions.ObjectNotFoundException;
 
@@ -27,6 +28,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 
